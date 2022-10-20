bbc24917e0e49ddf88472e08d5e42bb659a51634
Vladimir Trotsenko <vladimir.tr@musan.kz>
2022-10-20 17:59:29 +0600

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  sw: '/sw.js'
+});
 
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = () => {
+  const plugins = [withPWA];
+
+  return plugins.reduce((acc, next) => next(acc));
+};
